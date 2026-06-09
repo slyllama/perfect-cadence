5329eba16b729de5ac5733bc54f88cfd5337e057
Alex Laing <alex.laing@me.com>
2026-06-09 12:07:09 +1200

    Add camera orbiting

--- lib/_global/utils/utils.gd
+++ lib/_global/utils/utils.gd
@@ -9,7 +9,6 @@ func crit_lerp(speed: float) -> float:
 func crit_plerp(speed: float) -> float:
 	return(clamp(1.0 - exp(-speed * _pdelta), 0.0, 1.0))
 
-# Get all children recursively
 func get_all_children(node: Node, arr := []) -> Array:
 	arr.push_back(node)
 	for _child in node.get_children():
@@ -17,18 +16,8 @@ func get_all_children(node: Node, arr := []) -> Array:
 	return(arr)
 
 func get_window_center() -> Vector2:
-	return(get_window().size / 2.0 / get_window().content_scale_factor)
-
-# Convert a vector in string format "X, Y, Z" to a typed Vector3
-func str_to_vec3(string: String) -> Vector3:
-	var _a: PackedStringArray = string.split(", ")
-	if _a.size() != 3: return(Vector3.ZERO) # return 0 if the input size isn't right
-	return(Vector3(_a[0].to_float(), _a[1].to_float(), _a[2].to_float()))
-
-func vec3_to_str(vec: Vector3) -> String:
-	return(str(snapped(vec.x, 0.01)) + ", "
-		+ str(snapped(vec.y, 0.01)) + ", "
-		+ str(snapped(vec.z, 0.01)))
+	var _scale := get_window().content_scale_factor
+	return(get_window().size / 2.0 / _scale)
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
--- lib/player/scripts/orbit.gd
+++ lib/player/scripts/orbit.gd
@@ -1,19 +1,32 @@
 extends Marker3D
 
+@export var orbit_sensitivity = 0.25
 var event_relative := Vector2.ZERO
 
 func _ready() -> void:
+	# Clear input capture on window focus loss
+	get_window().focus_exited.connect(func() -> void:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if Input.is_action_just_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
-	if event is InputEventMouseMotion:
-		event_relative = event.relative
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			event_relative = event.relative
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	var _last_event_relative = event_relative
-	# Panning logic goes in here
+	
+	# Apply and clamp orbiting
+	rotation.y -= event_relative.x * delta * orbit_sensitivity
+	rotation.x -= event_relative.y * delta * orbit_sensitivity
+	rotation.x = clamp(
+		rotation.x, -PI / 2.0 + 0.1, PI / 2.0 - 0.1)
+	
 	if event_relative == _last_event_relative:
 		event_relative = Vector2.ZERO
